9d074aaa8bf54e95981250be3ea18ed0659ff295
deleitempanadas <deleite.empanadas2012@gmail.com>
2024-03-05 17:27:19 -0600

    four commit

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
-	year: 2023, // required
-	month: 12, // required
-	day: 15, // required
-	hours: 11, // Default is 0 [0-23] integer
+	year: 2024, // required
+	month: 4, // required
+	day: 5, // required
+	hours: 15, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
